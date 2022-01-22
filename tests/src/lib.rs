f63a2ff49eb00df40775ef4d42bc0b12f6528df3
Nim Siriwardana <nim@metacask.com>
2022-01-22 11:57:05 +0100

    adding a name parameter for the kyc proxy contract

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,12 +1,19 @@
 use std::collections::BTreeMap;
+use std::path::PathBuf;
 
-use casper_engine_test_support::{Code, Hash, SessionBuilder, TestContext, TestContextBuilder};
+use casper_engine_test_support::{
+    DeployItemBuilder, ExecuteRequestBuilder, InMemoryWasmTestBuilder, WasmTestBuilder, ARG_AMOUNT,
+    DEFAULT_ACCOUNT_ADDR, DEFAULT_PAYMENT, DEFAULT_RUN_GENESIS_REQUEST,
+};
+
+use casper_execution_engine::storage::global_state::in_memory::InMemoryGlobalState;
+use casper_types::system::mint;
 use casper_types::{account::AccountHash, runtime_args, PublicKey, RuntimeArgs, SecretKey, U512};
 use casper_types::{ContractHash, ContractPackageHash, Key};
 
 pub struct ProxyContract {
-    pub context: TestContext,
-    pub contract_hash: Hash,
+    pub builder: WasmTestBuilder<InMemoryGlobalState>,
+    pub contract_hash: [u8; 32],
     pub package_hash: ContractPackageHash,
     pub admin_account: (PublicKey, AccountHash),
     pub participant_two: (PublicKey, AccountHash),
@@ -28,43 +35,119 @@ impl ProxyContract {
         let participant_two_account_addr = AccountHash::from(&participant_two_public_key);
         let participant_three_account_addr = AccountHash::from(&participant_three_public_key);
 
-        // create context with cash for all users
-        let clx_init_balance = U512::from(500_000_000_000_000_000u64);
-        let mut context = TestContextBuilder::new()
-            .with_public_key(admin_public_key.clone(), clx_init_balance)
-            .with_public_key(participant_two_public_key.clone(), clx_init_balance)
-            .with_public_key(participant_three_public_key.clone(), clx_init_balance)
-            .build();
-
-        // load contract into context
-        let code = Code::from("kyc-proxy.wasm");
-        let args = runtime_args! {"initial_providers"=> Option::<Vec<ContractPackageHash>>::None};
-        let session = SessionBuilder::new(code, args)
+        let code = PathBuf::from("kyc-proxy.wasm");
+        let args = runtime_args! {"name"=> "kyc", "initial_providers"=> Option::<Vec<ContractPackageHash>>::None};
+        let deploy = DeployItemBuilder::new()
+            .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => *DEFAULT_PAYMENT})
+            .with_session_code(code, args)
             .with_address(admin_account_addr)
             .with_authorization_keys(&[admin_account_addr])
             .build();
-        context.run(session);
+        let execute_request = ExecuteRequestBuilder::from_deploy_item(deploy).build();
 
-        let contract_hash = context
-            .query(admin_account_addr, &["kyc-proxy_contract_hash".to_string()])
-            .unwrap_or_else(|_| panic!("kyc-proxy_contract_hash contract not found"))
-            .into_t()
-            .unwrap_or_else(|_| panic!("kyc-proxy_contract_hash has wrong type"));
+        let mut builder = InMemoryWasmTestBuilder::default();
+        builder.run_genesis(&DEFAULT_RUN_GENESIS_REQUEST).commit();
 
-        let package_hash: ContractPackageHash = context
+        let fund_my_account_request = {
+            let deploy_item = DeployItemBuilder::new()
+                .with_address(*DEFAULT_ACCOUNT_ADDR)
+                .with_authorization_keys(&[*DEFAULT_ACCOUNT_ADDR])
+                .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => *DEFAULT_PAYMENT})
+                .with_transfer_args(runtime_args! {
+                    mint::ARG_AMOUNT => U512::from(30_000_000_000_000_u64),
+                    mint::ARG_TARGET => admin_public_key.clone(),
+                    mint::ARG_ID => <Option::<u64>>::None
+                })
+                .with_deploy_hash([1; 32])
+                .build();
+
+            ExecuteRequestBuilder::from_deploy_item(deploy_item).build()
+        };
+        builder
+            .exec(fund_my_account_request)
+            .expect_success()
+            .commit();
+
+        let fund_my_account_request = {
+            let deploy_item = DeployItemBuilder::new()
+                .with_address(*DEFAULT_ACCOUNT_ADDR)
+                .with_authorization_keys(&[*DEFAULT_ACCOUNT_ADDR])
+                .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => *DEFAULT_PAYMENT})
+                .with_transfer_args(runtime_args! {
+                    mint::ARG_AMOUNT => U512::from(30_000_000_000_000_u64),
+                    mint::ARG_TARGET => participant_two_public_key.clone(),
+                    mint::ARG_ID => <Option::<u64>>::None
+                })
+                .with_deploy_hash([1; 32])
+                .build();
+
+            ExecuteRequestBuilder::from_deploy_item(deploy_item).build()
+        };
+        builder
+            .exec(fund_my_account_request)
+            .expect_success()
+            .commit();
+
+        let fund_my_account_request = {
+            let deploy_item = DeployItemBuilder::new()
+                .with_address(*DEFAULT_ACCOUNT_ADDR)
+                .with_authorization_keys(&[*DEFAULT_ACCOUNT_ADDR])
+                .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => *DEFAULT_PAYMENT})
+                .with_transfer_args(runtime_args! {
+                    mint::ARG_AMOUNT => U512::from(30_000_000_000_000_u64),
+                    mint::ARG_TARGET => participant_three_public_key.clone(),
+                    mint::ARG_ID => <Option::<u64>>::None
+                })
+                .with_deploy_hash([1; 32])
+                .build();
+
+            ExecuteRequestBuilder::from_deploy_item(deploy_item).build()
+        };
+        builder
+            .exec(fund_my_account_request)
+            .expect_success()
+            .commit();
+
+        builder.exec(execute_request).commit().expect_success();
+
+        // make assertions
+        let contract_hash = builder
             .query(
-                admin_account_addr,
+                None,
+                Key::Account(admin_account_addr),
+                &["kyc-proxy_contract_hash".to_string()],
+            )
+            .expect("should be stored value.")
+            .as_cl_value()
+            .expect("should be cl value.")
+            .clone()
+            .into_t()
+            .expect("should be string.");
+        let package_hash = builder
+            .query(
+                None,
+                Key::Account(admin_account_addr),
                 &[
                     "kyc-proxy_contract".to_string(),
                     "kyc-proxy_contract_package".to_string(),
                 ],
             )
-            .unwrap()
+            .expect("should be stored value.")
+            .as_cl_value()
+            .expect("should be cl value.")
+            .clone()
             .into_t()
-            .unwrap();
+            .expect("should be string.");
+
+        // let clx_init_balance = U512::from(500_000_000_000_000_000u64);
+        // let mut context = DeployItemBuilder::default()
+        //     .with_public_key(admin_public_key.clone(), clx_init_balance)
+        //     .with_public_key(participant_two_public_key.clone(), clx_init_balance)
+        //     .with_public_key(participant_three_public_key.clone(), clx_init_balance)
+        //     .build();
 
         Self {
-            context,
+            builder,
             contract_hash,
             package_hash,
             admin_account: (admin_public_key, admin_account_addr),
@@ -78,7 +161,7 @@ impl ProxyContract {
         deployer: AccountHash,
         kyc_name: &str,
     ) -> (ContractPackageHash, ContractHash) {
-        let kyc_code = Code::from("kyc-contract.wasm");
+        let kyc_code = PathBuf::from("kyc-contract.wasm");
         let mut meta = BTreeMap::new();
         meta.insert("origin".to_string(), "kyc".to_string());
 
@@ -89,28 +172,43 @@ impl ProxyContract {
             "meta" => meta,
             "admin" => Key::Account(deployer)
         };
-        let kyc_session = SessionBuilder::new(kyc_code, kyc_args)
+        let kyc_session = DeployItemBuilder::new()
+            .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => *DEFAULT_PAYMENT})
+            .with_session_code(kyc_code, kyc_args)
             .with_address(deployer)
             .with_authorization_keys(&[deployer])
             .build();
-
-        self.context.run(kyc_session);
+        let execute_request = ExecuteRequestBuilder::from_deploy_item(kyc_session).build();
+        self.builder.exec(execute_request).commit();
         (
-            self.context
-                .query(deployer, &["kyc_package_hash_wrapped".to_string()])
-                .unwrap()
+            self.builder
+                .query(
+                    None,
+                    Key::Account(deployer),
+                    &["kyc_package_hash_wrapped".to_string()],
+                )
+                .expect("should be stored value.")
+                .as_cl_value()
+                .expect("should be cl value.")
+                .clone()
                 .into_t()
-                .unwrap(),
-            self.context
-                .query(deployer, &["kyc_contract_hash_wrapped".to_string()])
-                .unwrap()
+                .expect("should be string."),
+            self.builder
+                .query(
+                    None,
+                    Key::Account(deployer),
+                    &["kyc_contract_hash_wrapped".to_string()],
+                )
+                .expect("should be stored value.")
+                .as_cl_value()
+                .expect("should be cl value.")
+                .clone()
                 .into_t()
-                .unwrap(),
+                .expect("should be string."),
         )
     }
 
-    pub fn add_kyc(&mut self, deployer: AccountHash, kyc_hash: Hash, recipient: AccountHash) {
-        let code = Code::Hash(kyc_hash, "mint".to_string());
+    pub fn add_kyc(&mut self, deployer: AccountHash, kyc_hash: [u8; 32], recipient: AccountHash) {
         let mut token_meta = BTreeMap::new();
         token_meta.insert("status".to_string(), "active".to_string());
         let args = runtime_args! {
@@ -118,17 +216,23 @@ impl ProxyContract {
             "token_id" => Some(recipient.to_string()),
             "token_meta" => token_meta
         };
-        let session = SessionBuilder::new(code, args)
+        let deploy = DeployItemBuilder::new()
+            .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => *DEFAULT_PAYMENT})
+            .with_stored_versioned_contract_by_hash(kyc_hash, None, "mint", args)
             .with_address(deployer)
             .with_authorization_keys(&[deployer])
             .build();
-        self.context.run(session);
+        let execute_request = ExecuteRequestBuilder::from_deploy_item(deploy).build();
+        self.builder.exec(execute_request).commit();
     }
 
     /// Getter function for the balance of an account.
     fn get_balance(&self, account_key: &AccountHash) -> U512 {
-        let main_purse_address = self.context.main_purse_address(*account_key).unwrap();
-        self.context.get_balance(main_purse_address.addr())
+        let account = self
+            .builder
+            .get_account(*account_key)
+            .expect("should get genesis account");
+        self.builder.get_purse_balance(account.main_purse())
     }
 
     /// Shorthand to get the balances of all 3 accounts in order.
@@ -142,12 +246,14 @@ impl ProxyContract {
 
     /// Function that handles the creation and running of sessions.
     fn call(&mut self, caller: AccountHash, method: &str, args: RuntimeArgs) {
-        let code = Code::Hash(self.contract_hash, method.to_string());
-        let session = SessionBuilder::new(code, args)
+        let deploy = DeployItemBuilder::new()
+            .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => *DEFAULT_PAYMENT})
+            .with_stored_versioned_contract_by_hash(self.contract_hash, None, method, args)
             .with_address(caller)
             .with_authorization_keys(&[caller])
             .build();
-        self.context.run(session);
+        let execute_request = ExecuteRequestBuilder::from_deploy_item(deploy).build();
+        self.builder.exec(execute_request).commit();
     }
 
     pub fn add_kyc_provider(&mut self, provider_package_hash_key: ContractPackageHash) {
@@ -174,19 +280,22 @@ impl ProxyContract {
         );
     }
 
-    pub fn is_kyc_proved(&mut self, result: bool) {
-        let code = Code::from("test_contract.wasm");
-        let session = SessionBuilder::new(
-            code,
-            runtime_args! {
-                "kyc_proxy_package_hash"=>self.package_hash,
-                "result" => result
-            },
-        )
-        .with_address(self.admin_account.1)
-        .with_authorization_keys(&[self.admin_account.1])
-        .build();
-        self.context.run(session);
+    pub fn is_kyc_proved(&mut self, result: bool) -> &mut WasmTestBuilder<InMemoryGlobalState> {
+        let code = PathBuf::from("test_contract.wasm");
+        let deploy = DeployItemBuilder::new()
+            .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => *DEFAULT_PAYMENT})
+            .with_session_code(
+                code,
+                runtime_args! {
+                    "kyc_proxy_package_hash"=>self.package_hash,
+                    "result" => result
+                },
+            )
+            .with_address(self.admin_account.1)
+            .with_authorization_keys(&[self.admin_account.1])
+            .build();
+        let execute_request = ExecuteRequestBuilder::from_deploy_item(deploy).build();
+        self.builder.exec(execute_request).commit()
     }
 }
 
@@ -205,7 +314,7 @@ fn test_no_provider() {
 #[should_panic = "User(999)"]
 fn test_no_provider_failing() {
     let mut proxy = ProxyContract::deploy();
-    proxy.is_kyc_proved(true);
+    proxy.is_kyc_proved(true).expect_success();
 }
 
 #[test]
